ebb2e10f871b7fdcfa559c04846ce47ea0d56976
Johannes Schobel <johannes.schobel@googlemail.com>
2020-02-07 13:40:31 +0100

    chore: add web and api aps

---
+++ apps/feelback-web/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'feelback-web',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/feelback-web',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
+  ]
+};
