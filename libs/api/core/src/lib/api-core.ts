cd1cf0de8a6efd4de33e6477cc9501fb0200055a
Johannes Schobel <johannes.schobel@googlemail.com>
2020-02-07 16:44:24 +0100

    feat: add few more core classes

--- libs/api/core/src/lib/api-core.ts
+++ libs/api/core/src/lib/api-core.ts
@@ -6,6 +6,10 @@ export * from './data/entities/versionable.entity';
 
 export * from './data/models/core.model';
 
+export * from './ui/graphql/args/core.args';
+
+export * from './ui/graphql/inputs/core.input';
+
 export * from './ui/graphql/objects/core.object';
 export * from './ui/graphql/objects/identifiable.object';
 export * from './ui/graphql/objects/versionable.object';
---
+++ libs/api/core/src/lib/ui/graphql/inputs/core.input.ts
@@ -0,0 +1,6 @@
+import { InputType } from 'type-graphql';
+
+@InputType({
+  isAbstract: true
+})
+export abstract class CoreInput {}
