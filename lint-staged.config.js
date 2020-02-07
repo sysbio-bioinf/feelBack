7f65a3ede86f3d13ace2a653734b14bcb664c05c
Johannes Schobel <johannes.schobel@googlemail.com>
2020-02-07 13:58:58 +0100

    chore: adding lots of packages

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  '{apps,libs}/**/*.{ts,json,md,scss,html}': files => {
+    return [
+      `npm run affected:lint -- --fix --parallel --files=${files.join(',')}`,
+      'npm run format:write -- --uncommitted',
+      `git add ${files.join(' ')}`
+    ];
+  }
+};
