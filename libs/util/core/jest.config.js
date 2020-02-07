e0fa3fcd74d1bec079eb606cc3bcebc0af57ea69
Johannes Schobel <johannes.schobel@googlemail.com>
2020-02-07 14:37:42 +0100

    chore: adding packages

--- libs/util/core/jest.config.js
+++ libs/util/core/jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   name: 'util-core',
   preset: '../../../jest.config.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../../coverage/libs/util/core',
+  coverageDirectory: '../../../coverage/libs/util/core'
 };
