af4fe6bdcd866beb7e9290c4431335032a64c2b3
Johannes Schobel <johannes.schobel@googlemail.com>
2020-02-19 11:06:08 +0100

    fix: make gql call to other service

--- apps/feelback-api/src/app/modules/person/ui/graphql/resolvers/person.resolver.ts
+++ apps/feelback-api/src/app/modules/person/ui/graphql/resolvers/person.resolver.ts
@@ -5,7 +5,9 @@ import { PersonService } from '../../../services/person.service';
 import { CreatePersonInput } from '../inputs/create-person.input';
 import { UpdatePersonInput } from '../inputs/update-person.input';
 import { PersonObject } from '../objects/person.object';
-import { HttpService } from '@nestjs/common';
+import { HttpService, InternalServerErrorException } from '@nestjs/common';
+import { print } from 'graphql';
+import { CreateOneIdentityDocument } from '../../../../../generated/feelback-identity.graphql';
 
 @ArgsType()
 export class CreateOnePersonArgsType extends CreateOneArgsType(
@@ -29,39 +31,26 @@ export class PersonResolver extends CRUDResolver(PersonObject, {
     super(service);
   }
 
-  // custom CREATE method
   @Mutation(() => PersonObject, { name: 'createOnePerson' })
   async createOnePerson(
     @Args() input: CreateOnePersonArgsType
   ): Promise<PersonObject> {
-    console.log('bla');
-
-    const result = await this.httpService
+    const identityResponse = await this.httpService
       .post('http://localhost:3001/graphql', {
-        query: `query identities {
-            identities {
-              pageInfo {
-                hasNextPage
-                hasPreviousPage
-                startCursor
-                endCursor
-              }
-              edges {
-                cursor
-                node {
-                  id
-                  pseudonym
-                  title
-                  firstname
-                  lastname
-                }
-              }
-            }
-          }`
+        query: print(CreateOneIdentityDocument),
+        variables: {
+          pseudonym: input.input.pseudonym
+        }
       })
       .toPromise();
 
-    console.log(result.data);
+    if (identityResponse.data.errors) {
+      // something went wrong, because we got an error back!
+      throw new InternalServerErrorException({
+        service: 'feelback-identity',
+        message: 'Failed to create identity'
+      });
+    }
 
     return super.createOne(input);
   }
