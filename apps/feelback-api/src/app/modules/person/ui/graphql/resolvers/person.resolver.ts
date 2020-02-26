14f83d18d001d93a9541ad045887c95c1506ba39
Johannes Schobel <johannes.schobel@googlemail.com>
2020-02-26 11:03:04 +0100

    chore: start adapting custom inputs to new query version

--- apps/feelback-api/src/app/modules/person/ui/graphql/resolvers/person.resolver.ts
+++ apps/feelback-api/src/app/modules/person/ui/graphql/resolvers/person.resolver.ts
@@ -1,16 +1,22 @@
-import { CreateOneArgsType, CRUDResolver } from '@nestjs-query/query-graphql';
+import { CRUDResolver, CreateOneInputType } from '@nestjs-query/query-graphql';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { ArgsType } from 'type-graphql';
 import { PersonService } from '../../../services/person.service';
 import { CreatePersonInput } from '../inputs/create-person.input';
 import { UpdatePersonInput } from '../inputs/update-person.input';
 import { PersonObject } from '../objects/person.object';
-import { HttpService, InternalServerErrorException } from '@nestjs/common';
+import {
+  HttpService,
+  InternalServerErrorException,
+  HttpStatus,
+} from '@nestjs/common';
 import { print } from 'graphql';
 import { CreateOneIdentityDocument } from '../../../../../generated/feelback-identity.graphql';
+import { InputType } from 'type-graphql';
+import { CoreException } from '@cancerlog/api/core';
 
-@ArgsType()
-export class CreateOnePersonArgsType extends CreateOneArgsType(
+@InputType()
+export class CreateOnePersonInputType extends CreateOneInputType(
+  PersonObject,
   CreatePersonInput,
 ) {}
 
@@ -20,6 +26,7 @@ export class PersonResolver extends CRUDResolver(PersonObject, {
     many: { disabled: true },
     one: { disabled: true },
     CreateDTOClass: CreatePersonInput,
+    CreateOneInput: CreateOnePersonInputType,
   },
   delete: { disabled: true },
   update: { many: { disabled: true }, UpdateDTOClass: UpdatePersonInput },
@@ -33,7 +40,7 @@ export class PersonResolver extends CRUDResolver(PersonObject, {
 
   @Mutation(() => PersonObject, { name: 'createOnePerson' })
   async createOnePerson(
-    @Args() input: CreateOnePersonArgsType,
+    @Args('input') input: CreateOnePersonInputType,
   ): Promise<PersonObject> {
     const identityResponse = await this.httpService
       .post('http://localhost:3001/graphql', {
@@ -46,12 +53,19 @@ export class PersonResolver extends CRUDResolver(PersonObject, {
 
     if (identityResponse.data.errors) {
       // something went wrong, because we got an error back!
-      throw new InternalServerErrorException({
-        service: 'feelback-identity',
-        message: 'Failed to create identity',
-      });
+      throw new CoreException(
+        {
+          source: {
+            pointer: 'feelback-identity',
+          },
+          detail: 'Failed to create identity',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
 
-    return super.createOne(input);
+    return null;
+    // FIXME: find a solution for this.
+    // return super.createOne(input);
   }
 }
