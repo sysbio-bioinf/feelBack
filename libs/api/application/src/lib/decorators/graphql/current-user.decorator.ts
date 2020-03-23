7471dd5984c237039b710d88557d5871d52ca844
Johannes Schobel <johannes.schobel@googlemail.com>
2020-03-23 12:58:14 +0100

    fix: update decorator

--- libs/api/application/src/lib/decorators/graphql/current-user.decorator.ts
+++ libs/api/application/src/lib/decorators/graphql/current-user.decorator.ts
@@ -1,5 +1,11 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const CurrentUser = createParamDecorator(
-  (data, [root, args, ctx, info]) => ctx.req.auth.user,
+  (data: unknown, context: ExecutionContext) => {
+    const gqlExecutionContext = GqlExecutionContext.create(context);
+    const gqlContext = gqlExecutionContext.getContext();
+
+    return gqlContext.req.auth.user;
+  },
 );
