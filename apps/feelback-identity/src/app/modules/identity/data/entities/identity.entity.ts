a7d1dc190dc757ef8dc1073c142e05bcca2a9439
Johannes Schobel <johannes.schobel@googlemail.com>
2020-03-30 13:46:58 +0200

    fix: fix typo in entity name

--- apps/feelback-identity/src/app/modules/identity/data/entities/identity.entity.ts
+++ apps/feelback-identity/src/app/modules/identity/data/entities/identity.entity.ts
@@ -1,7 +1,7 @@
 import { VersionableEntity } from '@cancerlog/api/core';
 import { Entity, Column, Index } from 'typeorm';
 
-@Entity({ name: 'identites' })
+@Entity({ name: 'identities' })
 export class IdentityEntity extends VersionableEntity {
   @Column({ type: 'varchar', length: 190, unique: true, nullable: false })
   @Index({ unique: true })
