08997dac7d912ba0cd52f1f6ad998f6a7339cf35
Johannes Schobel <johannes.schobel@googlemail.com>
2020-04-18 14:18:24 +0200

    feat(mobile): add qr code scanner

--- apps/feelback-ionic/src/app/pages/login-qrcode/login-qrcode.module.ts
+++ apps/feelback-ionic/src/app/pages/login-qrcode/login-qrcode.module.ts
@@ -3,9 +3,15 @@ import { IonicModule } from '@ionic/angular';
 import { SharedModule } from 'src/app/modules/shared.module';
 import { LoginQrcodePageRoutingModule } from './login-qrcode-routing.module';
 import { LoginQrcodePage } from './login-qrcode.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  imports: [SharedModule, IonicModule, LoginQrcodePageRoutingModule],
+  imports: [
+    SharedModule,
+    IonicModule,
+    LoginQrcodePageRoutingModule,
+    TranslateModule.forChild(),
+  ],
   declarations: [LoginQrcodePage],
 })
 export class LoginQrcodePageModule {}
--- apps/feelback-ionic/src/app/pages/login-qrcode/login-qrcode.page.ts
+++ apps/feelback-ionic/src/app/pages/login-qrcode/login-qrcode.page.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractComponent } from 'src/app/core/components/abstract.component';
+import { Router } from '@angular/router';
+import {
+  BarcodeScanner,
+  BarcodeScannerOptions,
+} from '@ionic-native/barcode-scanner/ngx';
+import { TranslatePipe } from '@ngx-translate/core';
+import { UserService } from 'src/app/services/user.service';
+import { CardService } from 'src/app/services/card.service';
 
 @Component({
   selector: 'feelback-login-qrcode',
   templateUrl: './login-qrcode.page.html',
   styleUrls: ['./login-qrcode.page.scss'],
+  providers: [TranslatePipe],
 })
-export class LoginQrcodePage implements OnInit {
+export class LoginQrcodePage extends AbstractComponent implements OnInit {
+  card: any;
+  pseudonym: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private scanner: BarcodeScanner,
+    private cardService: CardService,
+    private translatePipe: TranslatePipe,
+    private userService: UserService,
+  ) {
+    super();
   }
 
+  ngOnInit() {}
+
+  openScanner() {
+    const barcodeScannerOptions: BarcodeScannerOptions = {
+      prompt: this.translatePipe.transform('app.pages.login.scanner.text'),
+      resultDisplayDuration: 0,
+      showFlipCameraButton: false,
+      showTorchButton: false,
+      torchOn: false,
+    };
+
+    this.scanner
+      .scan(barcodeScannerOptions)
+      .then(async (scannedData) => {
+        const card = this.cardService.readCard(scannedData.text);
+        this.pseudonym = card.pseudonym;
+      })
+      .catch((err) => {
+        console.log(err);
+        throw new Error(err);
+      });
+  }
+
+  login() {
+    this.pseudonym = this.pseudonym.trim();
+
+    if (!this.pseudonym || this.pseudonym.length === 0) {
+      console.log('error');
+    }
+
+    this.userService.loginWithPseudonym(this.pseudonym);
+
+    this.router.navigate(['main', 'home']);
+  }
 }
