96a2759b61fc1dca5ef82e84649bbcb71e12e72a
Johannes Schobel <johannes.schobel@googlemail.com>
2020-04-22 09:46:11 +0200

    chore: bump some deps

--- apps/feelback-api/src/app/modules/person/ui/graphql/types/custom.types.ts
+++ apps/feelback-api/src/app/modules/person/ui/graphql/types/custom.types.ts
@@ -1,7 +1,6 @@
 import { CreateOneInputType } from '@nestjs-query/query-graphql';
 import { InputType } from '@nestjs/graphql';
 import { CreatePersonInput } from '../inputs/create-person.input';
-import { PersonObject } from '../objects/person.object';
 
 @InputType()
 export class CreateOnePersonInputType extends CreateOneInputType(
