13f0fbe216c8be70a25b0aad42b1161d2def65c7
Johannes Schobel <johannes.schobel@googlemail.com>
2020-04-23 14:52:39 +0200

    fix(web): fix routing issue

--- apps/feelback-web/src/app/app-routing.module.ts
+++ apps/feelback-web/src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { StartPage } from './pages/start/start.page';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'start', pathMatch: 'full' }, // redirect to the start page
   {
-    path: 'start',
+    path: '',
     loadChildren: () =>
       import('./pages/start/start.module').then((m) => m.StartModule),
   },
-  // { path: 'start', component: StartPage },
 ];
 
 @NgModule({
