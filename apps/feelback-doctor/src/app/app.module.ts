6e131929b69487ccfb0ca23ddc4fd40e6d269d4e
Dakesi95 <30556531+Dakesi95@users.noreply.github.com>
2020-04-24 11:59:34 +0200

    style(doctor): added material style

--- apps/feelback-doctor/src/app/app.module.ts
+++ apps/feelback-doctor/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
