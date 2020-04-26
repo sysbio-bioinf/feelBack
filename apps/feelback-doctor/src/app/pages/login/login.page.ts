dc9205cd4d80c63c9b090c6622ecfb22b8b1161d
Dakesi95 <30556531+Dakesi95@users.noreply.github.com>
2020-04-26 10:55:09 +0200

    feat(doctor): added basic login page

--- apps/feelback-doctor/src/app/pages/login/login.page.ts
+++ apps/feelback-doctor/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'feelback-doctor-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public login() {
+    this.router.navigateByUrl('/patient-list');
   }
 
 }
--- apps/feelback-doctor/src/app/pages/pages.module.ts
+++ apps/feelback-doctor/src/app/pages/pages.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PatientListPage } from './patient-list/patient-list.page';
 import { PatientDetailsPage } from './patient-details/patient-details.page';
 import { LoginPage } from './login/login.page';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -11,7 +16,12 @@ import { LoginPage } from './login/login.page';
     LoginPage
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule
   ]
 })
 export class PagesModule { }
