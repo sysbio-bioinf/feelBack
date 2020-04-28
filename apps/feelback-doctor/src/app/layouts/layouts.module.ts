4af3435c0b019c7c882cc1032c9bb6e443517027
Daniel <daniel.kempf@uni-ulm.de>
2020-04-28 11:24:28 +0200

    refactor(doctor): created shared module

--- apps/feelback-doctor/src/app/layouts/layouts.module.ts
+++ apps/feelback-doctor/src/app/layouts/layouts.module.ts
@@ -2,20 +2,12 @@ import { NgModule } from '@angular/core';
 import { LoginLayout } from './login/login.layout';
 import { MainLayout } from './main/main.layout';
 import { RouterModule } from '@angular/router';
-import { ComponentsModule } from '../components/components.module';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../modules/shared.module';
 
 @NgModule({
-  declarations: [
-    LoginLayout,
-    MainLayout
-  ],
-  imports: [
-    RouterModule,
-    ComponentsModule,
-    MatDividerModule,
-    CommonModule
-  ]
+  declarations: [LoginLayout, MainLayout],
+  imports: [RouterModule, SharedModule, MatDividerModule, CommonModule],
 })
-export class LayoutsModule { }
+export class LayoutsModule {}
