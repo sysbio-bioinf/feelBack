ec024e2be3a0b0fb6d892206e0c94fe3f7d747c4
Johannes Schobel <johannes.schobel@googlemail.com>
2020-04-30 18:11:01 +0200

    chore: update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,5 +17,19 @@ module.exports = {
         && npm run docker:build:api \
         && docker-compose -f prod.docker-compose.yml up -d',
     },
+
+    prod: {
+      key: '~/.ssh/id_rsa',
+      user: 'feelback',
+      host: ['134.60.71.108'],
+      ssh_options: 'ForwardAgent=yes',
+      ref: 'origin/master',
+      repo: 'git@github.com:johannesschobel/cancerlog.git',
+      path: '/home/feelback/wwwroot/apps/feelback-app',
+      'post-deploy':
+        'cp ./../../.env2 ./.env \
+        && npm run docker:prod:build \
+        && npm run docker:prod:serve',
+    },
   },
 };
