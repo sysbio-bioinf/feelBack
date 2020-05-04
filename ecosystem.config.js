3be3d0cfa916ccdd98b1e8f7a4f3d046742ffef1
Johannes Schobel <johannes.schobel@googlemail.com>
2020-05-04 12:52:24 +0200

    build: update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,22 +7,6 @@ module.exports = {
       user: 'feelback',
       host: ['134.60.71.108'],
       ssh_options: 'ForwardAgent=yes',
-      ref: 'origin/master',
-      repo: 'git@github.com:johannesschobel/cancerlog.git',
-      path: '/home/feelback/wwwroot/apps/feelback',
-      'post-deploy':
-        'cp ./../../.env ./.env \
-        && npm run docker:build:core \
-        && npm run docker:build:deps \
-        && npm run docker:build:api \
-        && docker-compose -f prod.docker-compose.yml up -d',
-    },
-
-    prod: {
-      key: '~/.ssh/id_rsa',
-      user: 'feelback',
-      host: ['134.60.71.108'],
-      ssh_options: 'ForwardAgent=yes',
       ref: 'origin/build/docker-01',
       repo: 'git@github.com:johannesschobel/cancerlog.git',
       path: '/home/feelback/wwwroot/apps/feelback-app',
