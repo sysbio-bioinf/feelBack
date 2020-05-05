ff55f9fa35b8f20375fef5d5a9ceeb90bab12be3
Johannes Schobel <johannes.schobel@googlemail.com>
2020-05-05 09:50:51 +0200

    refactor: move images

--- apps/feelback-ionic/src/app/pages/tutorial/tutorial.page.ts
+++ apps/feelback-ionic/src/app/pages/tutorial/tutorial.page.ts
@@ -14,25 +14,25 @@ export class TutorialPage extends AbstractComponent {
   // TODO Extract to I18N file
   tutorialSlides: TutorialSlideModel[] = [
     {
-      image: 'assets/imgs/tutorial/tutorial-01.png',
+      image: 'assets/images/tutorial/tutorial-01.png',
       title: 'Willkommen bei <b>FeelBack</b>',
       text:
         '<b>FeelBack</b> soll helfen, Tumorpatientinnen und -patienten eine bedarfsgerechte psychoonkologische Versorgung zu ermöglichen.',
     },
     {
-      image: 'assets/imgs/tutorial/tutorial-02.png',
+      image: 'assets/images/tutorial/tutorial-02.png',
       title: 'Was macht <b>FeelBack</b>?',
       text:
         'Bei <b>FeelBack</b> werden die Screeningdaten digital erfasst und in Form eines standardisierten Datensatzes sicher an eine zentrale Datenbank übertragen.',
     },
     {
-      image: 'assets/imgs/tutorial/tutorial-03.png',
+      image: 'assets/images/tutorial/tutorial-03.png',
       title: 'Was ist das Ziel von <b>FeelBack</b>?',
       text:
         '<b>FeelBack</b> ermöglicht die Identifikation psychosozial belasteter Patientinnen und Patienten, ebenso wie die Dokumentation des Screenings und des Therapieverlaufs.',
     },
     {
-      image: 'assets/imgs/tutorial/tutorial-04.png',
+      image: 'assets/images/tutorial/tutorial-04.png',
       title: 'Los Gehts!',
       text: 'FeelBack starten',
     },
