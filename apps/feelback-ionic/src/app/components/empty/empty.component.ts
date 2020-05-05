50e62b7580e790afe0f2eeeec25d3a26255c9d43
Johannes Schobel <johannes.schobel@googlemail.com>
2020-05-05 13:39:14 +0200

    feat(mobile): add empty component

---
+++ apps/feelback-ionic/src/app/components/empty/empty.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractComponent } from 'src/app/core/components/abstract.component';
+import { TranslatePipe } from '@ngx-translate/core';
+
+@Component({
+  selector: 'feelback-ionic-empty',
+  templateUrl: './empty.component.html',
+  styleUrls: ['./empty.component.scss'],
+  providers: [TranslatePipe],
+})
+export class EmptyComponent extends AbstractComponent implements OnInit {
+  constructor(readonly translatePipe: TranslatePipe) {
+    super();
+  }
+
+  ngOnInit() {}
+}
--- apps/feelback-ionic/src/app/modules/components.module.ts
+++ apps/feelback-ionic/src/app/modules/components.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgPipesModule } from 'ngx-pipes';
+import { EmptyComponent } from '../components/empty/empty.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { InstrumentCardComponent } from '../components/instrument-card/instrument-card.component';
 import { OrganizationCardComponent } from '../components/organization-card/organization-card.component';
 import { SurveyViewComponent } from '../components/survey-view/survey-view.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
-import { NgPipesModule } from 'ngx-pipes';
 
 const components = [
+  EmptyComponent,
   HeaderComponent,
   SurveyViewComponent,
   OrganizationCardComponent,
