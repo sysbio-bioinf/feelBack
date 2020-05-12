0114aa4faf3a469b3baf28d288a64592cbd7dae6
Johannes Schobel <johannes.schobel@googlemail.com>
2020-05-12 17:39:14 +0200

    style: adjust print document style

--- apps/feelback-ionic/src/app/components/survey-view/survey-view.component.ts
+++ apps/feelback-ionic/src/app/components/survey-view/survey-view.component.ts
@@ -175,29 +175,47 @@ export class SurveyViewComponent extends AbstractComponent implements OnInit {
 
       let resultText = '';
 
+      resultText = resultText + '<div style="font-size: 12px">';
+
       resultText = resultText + '<h1>' + this.instrument.name + '</h1>';
       resultText =
         resultText +
         `<p>${new Date().toLocaleString(
           this.translateService.currentLang,
         )}</p>`;
-      resultText = resultText + '<table>';
+
+      resultText = resultText + `<p>Name: _____________________________</p>`;
+      resultText = resultText + `<p>Geburtsdatum: _____________________</p>`;
+
+      resultText = resultText + '<table style="width: 100%; font-size: 1em">';
 
       let id = 0;
 
       for (const q of plainData) {
+        if (q.name.startsWith('__')) {
+          // we skip elements starting with "__" for our html export and print view
+          continue;
+        }
+
+        const question = q.title;
+        let value = q.displayValue;
+        if (value === '' || value === null || value === undefined) {
+          value = '---';
+        }
+
         id++;
         resultText =
           resultText +
           `<tr>
-          <td>${id}<td>
-          <td>${q.name}<td>
-          <td>${q.title}<td>
-          <td>${q.displayValue} (${q.value})<td>
+          <td style="width: 10%; vertical-align: top; border-bottom: 1px solid #7e8c8d;">${id}<td>
+          <td style="width: 40%; vertical-align: top; border-bottom: 1px solid #7e8c8d;">${question}<td>
+          <td style="width: 50%; vertical-align: top; border-bottom: 1px solid #7e8c8d;">${value}<td>
         </tr>`;
       }
 
       resultText = resultText + '</table>';
+      resultText = resultText + '</div>';
+
       this.printData = resultText;
 
       await this.storageService.writeDataToFile(filename + '.html', resultText);
@@ -248,14 +266,7 @@ export class SurveyViewComponent extends AbstractComponent implements OnInit {
 
     Survey.SurveyNG.render('surveyElement', {
       model: this.survey,
-      // onCurrentPageChanged: this.surveyPageChanged,
-      // onComplete: this.surveyOnComplete,
     });
-  }
-
-  private async surveyOnComplete(survey: Survey.Survey) {
-    // document.getElementById('printButton').style.visibility = 'visible';
-    // document.getElementById('navigateHome').style.visibility = 'visible';
   }
 
   printResult() {
