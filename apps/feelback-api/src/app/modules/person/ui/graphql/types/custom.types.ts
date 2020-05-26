9eb21a63d3f3685aebabde912269187e469c8d6a
Johannes Schobel <johannes.schobel@googlemail.com>
2020-05-26 13:45:24 +0200

    fix(api): cleanup

--- apps/feelback-api/src/app/modules/person/ui/graphql/types/custom.types.ts
+++
@@ -1,9 +0,0 @@
-import { CreateOneInputType } from '@nestjs-query/query-graphql';
-import { InputType } from '@nestjs/graphql';
-import { CreatePersonInput } from '../inputs/create-person.input';
-
-@InputType()
-export class CreateOnePersonInputType extends CreateOneInputType(
-  'person',
-  CreatePersonInput,
-) {}
