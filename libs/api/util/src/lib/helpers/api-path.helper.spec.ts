e3cc2832c7876834d2fa2666726983ab46bc5922
Johannes Schobel <johannes.schobel@googlemail.com>
2020-06-02 14:32:11 +0200

    test(api-util): make pass on windows

--- libs/api/util/src/lib/helpers/api-path.helper.spec.ts
+++ libs/api/util/src/lib/helpers/api-path.helper.spec.ts
@@ -1,36 +1,50 @@
 import { ApiPathHelper } from './api-path.helper';
+import * as path from 'path';
 
 describe('ApiPathHelper', () => {
   it('should create valid paths', async () => {
-    // TODO: make executable for windows
     const basePath = ApiPathHelper.basePath('foo');
-    expect(basePath).toContain('apps/foo');
+    expect(basePath).toContain(`apps${path.sep}foo`);
 
     const srcPath = ApiPathHelper.srcPath('foo');
-    expect(srcPath).toContain('apps/foo/src');
+    expect(srcPath).toContain(`apps${path.sep}foo${path.sep}src`);
 
     const appPath = ApiPathHelper.appPath('foo');
-    expect(appPath).toContain('apps/foo/src/app');
+    expect(appPath).toContain(`apps${path.sep}foo${path.sep}src${path.sep}app`);
 
     const modulesPath = ApiPathHelper.modulesPath('foo');
-    expect(modulesPath).toContain('apps/foo/src/app/modules');
+    expect(modulesPath).toContain(
+      `apps${path.sep}foo${path.sep}src${path.sep}app${path.sep}modules`,
+    );
 
     const moduleAPath = ApiPathHelper.modulePath('foo', 'a');
-    expect(moduleAPath).toContain('apps/foo/src/app/modules/a');
+    expect(moduleAPath).toContain(
+      `apps${path.sep}foo${path.sep}src${path.sep}app${path.sep}modules${path.sep}a`,
+    );
 
     const envPath = ApiPathHelper.envPath('foo');
-    expect(envPath).toContain('apps/foo/src/environments');
+    expect(envPath).toContain(
+      `apps${path.sep}foo${path.sep}src${path.sep}environments`,
+    );
 
     const assetsPath = ApiPathHelper.assetsPath('foo');
-    expect(assetsPath).toContain('apps/foo/src/assets');
+    expect(assetsPath).toContain(
+      `apps${path.sep}foo${path.sep}src${path.sep}assets`,
+    );
 
     const dbPath = ApiPathHelper.dbPath('foo');
-    expect(dbPath).toContain('apps/foo/src/database');
+    expect(dbPath).toContain(
+      `apps${path.sep}foo${path.sep}src${path.sep}database`,
+    );
 
     const migrationsPath = ApiPathHelper.migrationsPath('foo');
-    expect(migrationsPath).toContain('apps/foo/src/database/migrations');
+    expect(migrationsPath).toContain(
+      `apps${path.sep}foo${path.sep}src${path.sep}database${path.sep}migrations`,
+    );
 
     const seedsPath = ApiPathHelper.seedsPath('foo');
-    expect(seedsPath).toContain('apps/foo/src/database/seeds');
+    expect(seedsPath).toContain(
+      `apps${path.sep}foo${path.sep}src${path.sep}database${path.sep}seeds`,
+    );
   });
 });
