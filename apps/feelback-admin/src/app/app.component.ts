9176a3c967b3087cf9da1eaafb1a4510febaad4b
Johannes Schobel <johannes.schobel@googlemail.com>
2020-06-02 15:23:18 +0200

    refactor(admin): fix linting issues

--- apps/feelback-admin/src/app/app.component.ts
+++ apps/feelback-admin/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cancerlog-root',
+  selector: 'feelback-admin-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
