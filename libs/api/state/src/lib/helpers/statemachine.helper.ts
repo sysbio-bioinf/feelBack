f81426c2a9ab1c5c9644b621f46c87cc657cd022
Johannes Schobel <johannes.schobel@googlemail.com>
2020-06-08 11:59:11 +0200

    chore: make strict

--- libs/api/state/src/lib/helpers/statemachine.helper.ts
+++ libs/api/state/src/lib/helpers/statemachine.helper.ts
@@ -2,7 +2,7 @@ import { StateMachine, AnyEventObject, State, interpret } from 'xstate';
 
 export function startFSMFromState(
   machine: StateMachine<any, any, AnyEventObject, any>,
-  stateDefinition?: string,
+  stateDefinition: string,
 ) {
   const currentState = State.create(JSON.parse(stateDefinition));
   const resolvedState = machine.resolveState(currentState);
