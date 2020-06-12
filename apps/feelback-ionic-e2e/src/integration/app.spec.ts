f1c6e623b9c012431e1b8507b293adb8987e05d9
Johannes Schobel <johannes.schobel@googlemail.com>
2020-06-12 13:42:43 +0200

    test(mobile): add e2e test

--- apps/feelback-ionic-e2e/src/integration/app.spec.ts
+++ apps/feelback-ionic-e2e/src/integration/app.spec.ts
@@ -3,11 +3,7 @@ import { getGreeting } from '../support/app.po';
 describe('feelback-ionic', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to feelback-ionic!');
+  it('should show the login page', () => {
+    cy.get('ion-button').contains('Login');
   });
 });
