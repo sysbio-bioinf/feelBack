7539625ba57d4b0a20058e87c56ca42f97412ef4
Dakesi95 <30556531+Dakesi95@users.noreply.github.com>
2020-06-12 16:49:43 +0200

    fix(doctor): fixed failed test

--- apps/feelback-doctor/src/app/layouts/app/pages/workflow/workflow.page.spec.ts
+++ apps/feelback-doctor/src/app/layouts/app/pages/workflow/workflow.page.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WorkflowPage } from './workflow.page';
+import { ComponentsModule } from 'apps/feelback-doctor/src/app/components/components.module';
+import { MaterialModule } from 'apps/feelback-doctor/src/app/material.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DatePipe } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('WorkflowPage', () => {
   let component: WorkflowPage;
@@ -8,7 +13,9 @@ describe('WorkflowPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WorkflowPage ]
+      declarations: [ WorkflowPage ],
+      imports: [ComponentsModule, MaterialModule, RouterTestingModule, BrowserAnimationsModule],
+      providers: [DatePipe]
     })
     .compileComponents();
   }));
