16095412331c7695f473f8564c52338faf11d24f
Johannes Schobel <johannes.schobel@googlemail.com>
2020-06-17 14:37:09 +0200

    test(ionic): e2e test

--- apps/feelback-ionic-e2e/src/integration/app.spec.ts
+++ apps/feelback-ionic-e2e/src/integration/app.spec.ts
@@ -1,9 +1,15 @@
 import { getGreeting } from '../support/app.po';
 
 describe('feelback-ionic', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {});
 
   it('should show the login page', () => {
-    cy.get('ion-button').contains('Login');
+    cy.visit('/');
+    cy.title().should('eq', 'FeelBack');
+  });
+
+  it('should check, if the tutorial button exists', () => {
+    cy.visit('/');
+    cy.get('[data-cy=tutorial-button]').should('contain.text', 'Open Tutorial');
   });
 });
