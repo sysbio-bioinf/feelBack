86119cda8b5957226da1cbb73867c1218618122e
Dakesi <dakesi95@gmail.com>
2020-06-19 12:59:37 +0200

    fix(doctor): fixed failing test

--- apps/feelback-doctor/src/app/components/util/action-bar/action-bar.component.spec.ts
+++ apps/feelback-doctor/src/app/components/util/action-bar/action-bar.component.spec.ts
@@ -3,18 +3,26 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActionBarComponent } from './action-bar.component';
 import { ComponentsModule } from '../../components.module';
 import { DatePipe } from '@angular/common';
+import { Apollo } from 'apollo-angular';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ScreeningService } from '../../../services/screening.service';
 
 describe('ActionBarComponent', () => {
   let component: ActionBarComponent;
   let fixture: ComponentFixture<ActionBarComponent>;
+  let screeningServiceStub: Partial<ScreeningService>;
+
+  screeningServiceStub = {
+    screenings: [],
+    index: 0
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ComponentsModule],
+      imports: [ComponentsModule, RouterTestingModule],
       declarations: [],
-      providers: [DatePipe]
-    })
-    .compileComponents();
+      providers: [DatePipe, Apollo, { provide: ScreeningService, useValue: screeningServiceStub }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
