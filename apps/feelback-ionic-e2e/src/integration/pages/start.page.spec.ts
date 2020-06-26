3ad158f642fbb31c98f928d61b15ae508abd52df
Johannes Schobel <johannes.schobel@googlemail.com>
2020-06-26 12:15:22 +0200

    test(mobile): add tests for start page

---
+++ apps/feelback-ionic-e2e/src/integration/pages/start.page.spec.ts
@@ -0,0 +1,32 @@
+import { LargeMobileDevice } from '@cancerlog/util/testing';
+
+describe('Start Page', () => {
+  let testingDevice: LargeMobileDevice;
+
+  before(() => {
+    testingDevice = new LargeMobileDevice();
+  });
+
+  beforeEach(() => {
+    cy.viewport(testingDevice.width, testingDevice.height);
+
+    cy.visit('/');
+  });
+
+  it('should show the login page', () => {
+    cy.title().should('eq', 'FeelBack');
+    cy.url().should('contain', '/start');
+
+    cy.get('[data-cy=header-title]').should('contain', 'FeelBack');
+  });
+
+  it('should check, if the login buttons exists', () => {
+    cy.get('[data-cy=button-login-code]').should('exist');
+    cy.get('[data-cy=button-login-anonymous]').should('exist');
+  });
+
+  it('should check, if the tutorial button exists', () => {
+    cy.get('[data-cy=button-tutorial]').should('exist');
+    cy.get('[data-cy=button-tutorial]').should('contain.text', 'Open Tutorial');
+  });
+});
