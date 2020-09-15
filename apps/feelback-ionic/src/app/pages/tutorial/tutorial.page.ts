4ebedc09d895549942a951bd4079b92e09773ce6
Benedikt Fels <benedikt.fels@uni-ulm.de>
2020-09-15 10:32:40 +0200

    Feat(Ionic): Tutorial Page Update
    - "Back" Button
    - content of the slides comes from the i18n json files now

--- apps/feelback-ionic/src/app/pages/tutorial/tutorial.page.ts
+++ apps/feelback-ionic/src/app/pages/tutorial/tutorial.page.ts
@@ -9,46 +9,14 @@ import { TutorialSlideModel } from '../../models/tutorial-slide.model';
   styleUrls: ['./tutorial.page.scss'],
 })
 export class TutorialPage extends AbstractComponent {
-  showSkip = true;
-
-  // TODO Extract to I18N file
-  tutorialSlides: TutorialSlideModel[] = [
-    {
-      image: 'assets/images/tutorial/tutorial-01.png',
-      title: 'Willkommen bei <b>FeelBack</b>',
-      text:
-        '<b>FeelBack</b> soll helfen, Tumorpatientinnen und -patienten eine bedarfsgerechte psychoonkologische Versorgung zu ermöglichen.',
-    },
-    {
-      image: 'assets/images/tutorial/tutorial-02.png',
-      title: 'Was macht <b>FeelBack</b>?',
-      text:
-        'Bei <b>FeelBack</b> werden die Screeningdaten digital erfasst und in Form eines standardisierten Datensatzes sicher an eine zentrale Datenbank übertragen.',
-    },
-    {
-      image: 'assets/images/tutorial/tutorial-03.png',
-      title: 'Was ist das Ziel von <b>FeelBack</b>?',
-      text:
-        '<b>FeelBack</b> ermöglicht die Identifikation psychosozial belasteter Patientinnen und Patienten, ebenso wie die Dokumentation des Screenings und des Therapieverlaufs.',
-    },
-    {
-      image: 'assets/images/tutorial/tutorial-04.png',
-      title: 'Los Gehts!',
-      text: 'FeelBack starten',
-    },
-  ];
+  numberOfSlides = 4;
+  slideNumberArray = Array.from(Array(this.numberOfSlides), (_, i) => i + 1);
 
   constructor(private router: Router) {
     super();
   }
 
-  onSlideChangeStart(event) {
-    event.target.isEnd().then((isEnd) => {
-      this.showSkip = !isEnd;
-    });
-  }
-
   async navigateToStart() {
-    await this.router.navigate(['start'], { replaceUrl: true });
+    await this.router.navigate(['/start'], { replaceUrl: true });
   }
 }
