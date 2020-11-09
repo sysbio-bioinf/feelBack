cfe0debb4aeecb3a441caf9985a50a8e9941a29b
Benedikt Fels <benedikt.fels@uni-ulm.de>
2020-11-09 10:07:57 +0100

    feat(Ionic): result service
    result service to generate the result text for a survey.

--- apps/feelback-ionic/src/app/components/survey-view/survey-view.component.ts
+++ apps/feelback-ionic/src/app/components/survey-view/survey-view.component.ts
@@ -10,6 +10,7 @@ import { Instrument } from '../../models/instrument.model';
 import { ScreeningService } from '../../services/api/screening.service';
 import { PrinterService } from '../../services/printer.service';
 import { StorageService } from '../../services/storage.service';
+import { ResultService } from '../../services/result.service';
 import { UserService } from '../../services/user.service';
 import * as Survey from 'survey-angular';
 import * as widgets from 'surveyjs-widgets';
@@ -42,6 +43,7 @@ export class SurveyViewComponent extends AbstractComponent implements OnInit {
     private userService: UserService,
     private screeningService: ScreeningService,
     private printerService: PrinterService,
+    private resultService: ResultService
   ) {
     super();
   }
@@ -148,48 +150,7 @@ export class SurveyViewComponent extends AbstractComponent implements OnInit {
 
       const plainData = survey.getPlainData({ includeEmpty: true });
 
-      let resultText = '';
-
-      resultText = resultText + '<div style="font-size: 12px">';
-
-      resultText = resultText + '<h1>' + this.instrument.name + '</h1>';
-      resultText =
-        resultText +
-        `<p>${new Date().toLocaleString(
-          this.translateService.currentLang,
-        )}</p>`;
-
-      resultText = resultText + `<p>Name: _____________________________</p>`;
-      resultText = resultText + `<p>Geburtsdatum: _____________________</p>`;
-
-      resultText = resultText + '<table style="width: 100%; font-size: 1em">';
-
-      let id = 0;
-
-      for (const q of plainData) {
-        if (q.name.startsWith('__')) {
-          // we skip elements starting with "__" for our html export and print view
-          continue;
-        }
-
-        const question = q.title;
-        let value = q.displayValue;
-        if (value === '' || value === null || value === undefined) {
-          value = '---';
-        }
-
-        id++;
-        resultText =
-          resultText +
-          `<tr>
-          <td style="width: 10%; vertical-align: top; border-bottom: 1px solid #7e8c8d;">${id}<td>
-          <td style="width: 40%; vertical-align: top; border-bottom: 1px solid #7e8c8d;">${question}<td>
-          <td style="width: 50%; vertical-align: top; border-bottom: 1px solid #7e8c8d;">${value}<td>
-        </tr>`;
-      }
-
-      resultText = resultText + '</table>';
-      resultText = resultText + '</div>';
+      const resultText = this.resultService.generateResultText(plainData, this.instrument.name)
 
       this.printData = resultText;
 
---
+++ apps/feelback-ionic/src/app/services/result.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ResultService {
+
+  constructor(private translateService: TranslateService) {}
+
+  generateResultText(plainData: any[], instrumentName: string) {
+    let resultText = '';
+
+    resultText = resultText + '<div style="font-size: 12px">';
+
+    resultText = resultText + '<h1>' + instrumentName + '</h1>';
+    resultText =
+    resultText +
+      `<p>${new Date().toLocaleString(
+        this.translateService.currentLang,
+      )}</p>`;
+
+    resultText = resultText + `<p>Name: _____________________________</p>`;
+    resultText = resultText + `<p>Geburtsdatum: _____________________</p>`;
+
+    resultText = resultText + '<table style="width: 100%; font-size: 1em">';
+
+    let id = 0;
+
+    for (const q of plainData) {
+      if (q.name.startsWith('__')) {
+        // we skip elements starting with "__" for our html export and print view
+        continue;
+      }
+
+      const question = q.title;
+      let value = q.displayValue;
+      if (value === '' || value === null || value === undefined) {
+        value = '---';
+      }
+
+      id++;
+      resultText =
+        resultText +
+          `<tr>
+          <td style="width: 10%; vertical-align: top; border-bottom: 1px solid #7e8c8d;">${id}<td>
+          <td style="width: 40%; vertical-align: top; border-bottom: 1px solid #7e8c8d;">${question}<td>
+          <td style="width: 50%; vertical-align: top; border-bottom: 1px solid #7e8c8d;">${value}<td>
+        </tr>`;
+    }
+
+    resultText = resultText + '</table>';
+    resultText = resultText + '</div>';
+
+    return resultText;
+  }
+
+}
