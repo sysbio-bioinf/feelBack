4b7fb55853589cdca7ba185eb09ff755c04b24df
Patrick Steiger <patrick.steiger@uni-ulm.de>
2020-12-29 13:57:55 +0100

    test(api): added tests for PersonDatabaseService

--- libs/api/person/src/lib/services/person-database.service.spec.ts
+++ libs/api/person/src/lib/services/person-database.service.spec.ts
@@ -1,8 +1,12 @@
 import { PersonEntity } from '@feelback-app/api/data';
-import { mockRepository } from '@feelback-app/api/testing';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { PersonDatabaseService } from './person-database.service';
+import { activePerson } from '@feelback-app/api/testing';
+
+const mockRepository = {
+  findOneOrFail: jest.fn((options?: any) => Promise.resolve(activePerson)),
+};
 
 describe('PersonDatabaseService', () => {
   let service: PersonDatabaseService;
@@ -13,7 +17,7 @@ describe('PersonDatabaseService', () => {
         PersonDatabaseService,
         {
           provide: getRepositoryToken(PersonEntity),
-          useClass: mockRepository,
+          useValue: mockRepository,
         },
       ],
     }).compile();
@@ -23,5 +27,15 @@ describe('PersonDatabaseService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getPersonByPseudonym', () => {
+    // Since getPersonByPseudonym has no logic for catching error produced by findOneOrFail, there won't be a test case checking for this.
+    it('should return by correct pseudonym', () => {
+      expect.assertions(1);
+      return expect(
+        service.getPersonByPseudonym(activePerson.pseudonym),
+      ).resolves.toStrictEqual(activePerson);
+    });
   });
 });
