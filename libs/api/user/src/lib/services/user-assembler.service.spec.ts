a3d72055f1b8cdf9d5e82a3da742fdf8bdb00e5f
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-01-11 22:52:28 +0100

    test(api): add tests for UserDatabaseService & UserAssemblerService

--- libs/api/user/src/lib/services/user-assembler.service.spec.ts
+++ libs/api/user/src/lib/services/user-assembler.service.spec.ts
@@ -1,15 +1,19 @@
 import { DoctorEntity } from '@feelback-app/api/data';
-import { mockRepository } from '@feelback-app/api/testing';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserAssembler } from '../ui/graphql/assemblers/user.assembler';
 import { UserAssemblerService } from './user-assembler.service';
 import { UserDatabaseService } from './user-database.service';
+import { mockDoctorEntity, mockRepository } from '@feelback-app/api/testing';
 
 describe('UserAssemblerService', () => {
   let service: UserAssemblerService;
+  let userDataBaseService: UserDatabaseService;
+  let userAssembler: UserAssembler;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserAssemblerService,
@@ -23,9 +27,35 @@ describe('UserAssemblerService', () => {
     }).compile();
 
     service = module.get<UserAssemblerService>(UserAssemblerService);
+    userAssembler = module.get<UserAssembler>(UserAssembler);
+    userDataBaseService = module.get<UserDatabaseService>(UserDatabaseService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(userDataBaseService).toBeDefined();
+  });
+
+  describe('getUserByKeycloakId', () => {
+    it('should return user object', async () => {
+      // Set mocks
+      const mockGetUserByKeycloakId = jest
+        .fn()
+        .mockResolvedValueOnce(mockDoctorEntity);
+      userDataBaseService.getUserByKeycloakId = mockGetUserByKeycloakId;
+      const mockConvertToDTO = jest.fn();
+      userAssembler.convertAsyncToDTO = mockConvertToDTO;
+      // Call method. Checking return value isn't needed.
+      await service.getUserByKeycloakId(mockDoctorEntity.keycloakId);
+      // Expect
+      expect(mockGetUserByKeycloakId).toBeCalledTimes(1);
+      expect(mockGetUserByKeycloakId).toBeCalledWith(
+        mockDoctorEntity.keycloakId,
+      );
+      expect(mockConvertToDTO).toBeCalledTimes(1);
+      expect(mockConvertToDTO).toBeCalledWith(
+        Promise.resolve(mockDoctorEntity),
+      );
+    });
   });
 });
--- libs/api/user/src/lib/services/user-database.service.spec.ts
+++ libs/api/user/src/lib/services/user-database.service.spec.ts
@@ -1,19 +1,25 @@
 import { DoctorEntity } from '@feelback-app/api/data';
-import { mockRepository } from '@feelback-app/api/testing';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserDatabaseService } from './user-database.service';
+
+const mockFindOneOrFail = jest.fn();
+const mockRepository = {
+  findOneOrFail: mockFindOneOrFail,
+};
 
 describe('UserDatabaseService', () => {
   let service: UserDatabaseService;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserDatabaseService,
         {
           provide: getRepositoryToken(DoctorEntity),
-          useClass: mockRepository,
+          useValue: mockRepository,
         },
       ],
     }).compile();
@@ -23,5 +29,21 @@ describe('UserDatabaseService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getUserByKeycloakId', () => {
+    const keycloakId = 'keycloakIdTest';
+    const expectedOptions = {
+      where: {
+        keycloakId: keycloakId,
+      },
+    };
+
+    // Since getUserByKeycloakId has no logic for catching error produced by findOneOrFail, there won't be a test case checking for this.
+    it('should return doctor', async () => {
+      await service.getUserByKeycloakId(keycloakId);
+      expect(mockFindOneOrFail).toBeCalledTimes(1);
+      expect(mockFindOneOrFail).toBeCalledWith(expectedOptions);
+    });
   });
 });
