308afb69edda0e22f0ae01d08f7f634403a3b67d
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-01-11 23:03:32 +0100

    test(api): add test for IdentityResolver

--- libs/api/identity/src/lib/ui/graphql/resolvers/identity.resolver.spec.ts
+++ libs/api/identity/src/lib/ui/graphql/resolvers/identity.resolver.spec.ts
@@ -1,7 +1,7 @@
 import { GuardsModule } from '@feelback-app/api/auth';
 import { IdentityEntity } from '@feelback-app/api/data';
 import { IDENTITY_DB_CONNECTION_NAME } from '@feelback-app/api/database';
-import { mockRepository } from '@feelback-app/api/testing';
+import { mockIdentityEntity } from '@feelback-app/api/testing';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { IdentityAssemblerService } from '../../../services/identity-assembler.service';
@@ -9,8 +9,14 @@ import { IdentityDatabaseService } from '../../../services/identity-database.ser
 import { IdentityAssembler } from '../assemblers/identity.assembler';
 import { IdentityResolver } from './identity.resolver';
 
+const mockFindOneOrFail = jest.fn();
+const mockRepository = {
+  findOneOrFail: mockFindOneOrFail,
+};
+
 describe('IdentityResolver', () => {
   let resolver: IdentityResolver;
+  let identityAssemblerService: IdentityAssemblerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -25,15 +31,42 @@ describe('IdentityResolver', () => {
             IdentityEntity,
             IDENTITY_DB_CONNECTION_NAME,
           ),
-          useClass: mockRepository,
+          useValue: mockRepository,
         },
       ],
     }).compile();
 
     resolver = module.get<IdentityResolver>(IdentityResolver);
+    identityAssemblerService = module.get<IdentityAssemblerService>(
+      IdentityAssemblerService,
+    );
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+    expect(identityAssemblerService).toBeDefined();
+  });
+
+  describe('identityByPseudonym', () => {
+    const pseudonym = 'input pseudonym';
+    const expectedOptions = {
+      where: {
+        pseudonym: pseudonym,
+      },
+    };
+
+    it('should return identity object', async () => {
+      // Set mocks
+      const mockConvertToDTO = jest.fn();
+      identityAssemblerService.assembler.convertToDTO = mockConvertToDTO;
+      mockFindOneOrFail.mockResolvedValue(mockIdentityEntity);
+      // Call method
+      await resolver.identityByPseudonym(pseudonym);
+      // Expect
+      expect(mockFindOneOrFail).toBeCalledTimes(1);
+      expect(mockFindOneOrFail).toBeCalledWith(expectedOptions);
+      expect(mockConvertToDTO).toBeCalledTimes(1);
+      expect(mockConvertToDTO).toBeCalledWith(mockIdentityEntity);
+    });
   });
 });
