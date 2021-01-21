f44453e9c8caf8784b2986912b7768023ecebaa2
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-01-21 13:58:13 +0100

    test(api): refactor mocks in UserAssemblerService

--- libs/api/user/src/lib/services/user-assembler.service.spec.ts
+++ libs/api/user/src/lib/services/user-assembler.service.spec.ts
@@ -1,15 +1,19 @@
 import { DoctorEntity } from '@feelback-app/api/data';
+import { DoctorObject } from '@feelback-app/api/interfaces';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserAssembler } from '../ui/graphql/assemblers/user.assembler';
 import { UserAssemblerService } from './user-assembler.service';
 import { UserDatabaseService } from './user-database.service';
-import { mockDoctorEntity, mockRepository } from '@feelback-app/api/testing';
+import { mockDoctorEntity } from '@feelback-app/api/testing';
+
+const mockFindOneOrFail: jest.Mock<Promise<DoctorEntity>> = jest.fn();
+const mockDoctorRepository = {
+  findOneOrFail: mockFindOneOrFail,
+};
 
 describe('UserAssemblerService', () => {
   let service: UserAssemblerService;
-  let userDataBaseService: UserDatabaseService;
-  let userAssembler: UserAssembler;
 
   beforeEach(async () => {
     jest.clearAllMocks();
@@ -21,41 +25,37 @@ describe('UserAssemblerService', () => {
         UserDatabaseService,
         {
           provide: getRepositoryToken(DoctorEntity),
-          useClass: mockRepository,
+          useValue: mockDoctorRepository,
         },
       ],
     }).compile();
 
     service = module.get<UserAssemblerService>(UserAssemblerService);
-    userAssembler = module.get<UserAssembler>(UserAssembler);
-    userDataBaseService = module.get<UserDatabaseService>(UserDatabaseService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-    expect(userDataBaseService).toBeDefined();
   });
 
   describe('getUserByKeycloakId', () => {
+    const expectedOptions = {
+      where: {
+        keycloakId: mockDoctorEntity.keycloakId,
+      },
+    };
+
     it('should return user object', async () => {
-      // Set mocks
-      const mockGetUserByKeycloakId = jest
-        .fn()
-        .mockResolvedValueOnce(mockDoctorEntity);
-      userDataBaseService.getUserByKeycloakId = mockGetUserByKeycloakId;
-      const mockConvertToDTO = jest.fn();
-      userAssembler.convertAsyncToDTO = mockConvertToDTO;
-      // Call method. Checking return value isn't needed.
-      await service.getUserByKeycloakId(mockDoctorEntity.keycloakId);
-      // Expect
-      expect(mockGetUserByKeycloakId).toBeCalledTimes(1);
-      expect(mockGetUserByKeycloakId).toBeCalledWith(
+      // Set mock to return DoctorEntity
+      mockFindOneOrFail.mockResolvedValueOnce(mockDoctorEntity);
+      // Call method
+      const result = await service.getUserByKeycloakId(
         mockDoctorEntity.keycloakId,
       );
-      expect(mockConvertToDTO).toBeCalledTimes(1);
-      expect(mockConvertToDTO).toBeCalledWith(
-        Promise.resolve(mockDoctorEntity),
-      );
+      // Expect
+      expect(result).toBeInstanceOf(DoctorObject);
+      expect(result).toMatchObject<DoctorObject>({ ...mockDoctorEntity });
+      expect(mockFindOneOrFail).toBeCalledTimes(1);
+      expect(mockFindOneOrFail).toBeCalledWith(expectedOptions);
     });
   });
 });
