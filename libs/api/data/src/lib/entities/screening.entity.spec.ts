a304f5fd1c92eec83ea20871a74b2accb35bd4c8
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-01-21 13:47:30 +0100

    test(api): add tests for getScreeningData of ScreeningEntity

--- libs/api/data/src/lib/entities/screening.entity.spec.ts
+++ libs/api/data/src/lib/entities/screening.entity.spec.ts
@@ -1,7 +1,26 @@
 import { ScreeningEntity } from './screening.entity';
 
 describe('ScreeningEntity', () => {
+  const screening = new ScreeningEntity();
+
   it('should be defined', () => {
-    expect(new ScreeningEntity()).toBeDefined();
+    expect(screening).toBeDefined();
+  });
+
+  describe('getScreeningData', () => {
+    it('should return undefined on simple initialization', () => {
+      expect(screening.getScreeningData()).toBeUndefined();
+    });
+
+    it('should return payload object after value is set', () => {
+      screening.payload = {};
+      expect(screening.getScreeningData()).toStrictEqual({});
+      const obj = {
+        foo: true,
+        bar: 5,
+      };
+      screening.payload = obj;
+      expect(screening.getScreeningData()).toStrictEqual(obj);
+    });
   });
 });
